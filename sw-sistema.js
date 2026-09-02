8fc3a3ca7afe2d4b87ea9c864048cd593326bdd8
encontrevoos-adms <encontrevoos@gmail.com>
2026-09-02 02:36:18 +0100

    Add files via upload

--- sw-sistema.js
+++ sw-sistema.js
@@ -1,1 +1,4 @@
-const CACHE='adms-sistema-v20260902-gabinete11';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))})
+const CACHE='adms-sistema-v20260902-lidergab12';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
